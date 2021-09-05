c35523f3ddf94895b2d072f0df0eca9f8419c189
doohpark <21800266@handong.edu>
2021-09-05 17:04:46 +0900

    first commit

---
+++ Main/src/Test/HelloWorld.java
@@ -0,0 +1,13 @@
+package Test;
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello World!!\n");
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("[" + name + "]님 환영합니다~");
+	}
+
+}
